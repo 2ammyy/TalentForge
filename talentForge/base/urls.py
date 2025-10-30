7b16dc7dc360d3d22c571b90c0c2b61cdf4e8656
Amira Hamdani <mirahmd318@gmail.com>
2025-10-30 20:26:21 +0100

    delete account either by username or Gmail with delete verification code OK

--- talentForge/base/urls.py
+++ talentForge/base/urls.py
@@ -1,5 +1,6 @@
+# Dans base/urls.py
 from django.urls import path
-from .views import authView, home, custom_login, verify_email, edit_profile, view_profile, delete_profile, profile_settings
+from .views import authView, home, custom_login, verify_email, edit_profile, view_profile, delete_profile, profile_settings, confirm_delete_profile, resend_delete_code
 from django.contrib.auth.views import LogoutView
 from .views import social_signup_redirect
 
@@ -15,6 +16,8 @@ urlpatterns = [
     path('profile/', view_profile, name='view_profile'),
     path('profile/settings/', profile_settings, name='profile_settings'),
     path('profile/delete/', delete_profile, name='delete_profile'),
+    path('profile/delete/confirm/', confirm_delete_profile, name='confirm_delete_profile'),
+    path('profile/delete/resend-code/', resend_delete_code, name='resend_delete_code'),  # Nouvelle URL
     
     path('accounts/3rdparty/signup/', social_signup_redirect, name='social_signup_redirect'),
 ]
