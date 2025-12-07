3450a031f34c85fc67f77cc96c8b92bbb86d2a96
Amira Hamdani <mirahmd318@gmail.com>
2025-12-07 14:22:07 +0100

    màj Posts

--- talentForge/posts/urls.py
+++ talentForge/posts/urls.py
@@ -16,8 +16,7 @@ urlpatterns = [
     path('reaction/<int:post_id>/', views.add_reaction, name='add_reaction'),
     path('share/<int:post_id>/', views.share_post, name='share_post'),
     path('unshare/<int:post_id>/', views.unshare_post, name='unshare_post'),
-    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    
+path('post/<int:pk>/', views.post_detail, name='post_detail'),    
     # ============ REPORTS ============
     path('report/<int:post_id>/', views.report_post, name='report_post'),
     path('my-reports/', views.my_reports, name='my_reports'),
