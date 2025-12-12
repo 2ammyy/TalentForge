99f7ddf05be6c58092df48cfc29b5da287a85e50
Zahrat-ElOula <zahrayessine@gmail.com>
2025-12-12 09:53:38 +0100

    màj

--- talentForge/talentForge/settings.py
+++ talentForge/talentForge/settings.py
@@ -111,8 +111,8 @@ DATABASES = {
         'USER': 'django_user',
         'PASSWORD': 'DjangoSecurePass123!',
         #'HOST': 'localhost',
-        'HOST': '192.168.0.60',  # Amira ipv4 address Machine -- Server
-        #'HOST': '10.209.202.115',  #ip address Tekup
+        #'HOST': '192.168.0.60',  # Amira ipv4 address Machine -- Server
+        'HOST': '10.209.202.115',  #ip address Tekup
         'PORT': '5432',
     }
 }
